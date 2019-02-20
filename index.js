60009f8658a06f84d327e71e0b7ae85a8ac03268
kkoomin <minha.koo.9@gmail.com>
2019-02-20 23:26:38 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,50 @@ const questions = [
 ]
 
 let question;
+const questionEl = document.querySelector(".question-container")
+const answerBtns = document.querySelectorAll(".btn.lighten-2")
+const startBtn = document.querySelector("a.btn")
+
+function appendQuestion(question) {
+  questionEl.innerText = question.questionText;
+}
+
+function askQuestionThen(time) {
+  question = questions[0]
+  appendQuestion(question)
+    return new Promise((resolve, reject) => {
+      setTimeout(function () {
+        resolve(question)
+      }, time)
+    })
+}
+
+function removeQuestion() {
+  questionEl.innerText = ""
+}
+
+function askQuestionThenRemoveQuestion(time) {
+  return askQuestionThen(time)
+    .then (removeQuestion)
+}
+
+function trueAndFalseButtons() {
+  let btn_arr = []
+  answerBtns.forEach( btn => {
+    btn_arr.push(btn)
+  })
+  return btn_arr
+}
+
+function toggleTrueAndFalseButtons() {
+  for (const el of trueAndFalseButtons()) {
+    el.classList.toggle("hide")
+  }
+}
+
+function displayQuestionOnClick() {
+  return startBtn.addEventListener("click", (e) => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
