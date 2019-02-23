66d798ebeaef6b162006356f929982dce8f4dc90
studioshane <studioshane@gmail.com>
2019-02-23 16:08:27 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,66 @@ const questions = [
 ]
 
 let question;
+
+function displayQuestionOnClick() {
+  let btn = document.querySelector('a')
+   btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+
+
+function appendQuestion(question) {
+  let questionCont = document.querySelector('.question-container')
+  questionCont.innerHTML = question.questionText;
+}
+
+function askQuestionThen(time) {
+
+  question = questions[0]
+  appendQuestion(question)
+  return new Promise((resolve) => {
+    setTimeout(function () {
+      resolve(question)
+    }, time)
+  })
+}
+
+
+function removeQuestion() {
+  return new Promise(function () {
+    let questionCont = document.querySelector('.question-container')
+    questionCont.innerHTML = ""
+    toggleTrueAndFalseButtons()
+  })
+
+
+}
+
+function askQuestionThenRemoveQuestion(time) {
+
+  return askQuestionThen(time).then(removeQuestion)
+
+}
+
+
+function trueAndFalseButtons() {
+
+  let trueAndFalse = document.querySelector('.true-false-list').querySelectorAll('.btn')
+  return trueAndFalse
+  // let falseBtn = document.querySelector('.red')
+
+}
+
+
+function toggleTrueAndFalseButtons() {
+  trueAndFalseButtons().forEach(function (btn) {
+    btn.classList.toggle("hide")
+  })
+}
+
+function checkQuestion(question, answer) {
+  question.questionAnswer == answer
+}
+
